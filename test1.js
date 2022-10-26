bf21e7565f98b2be861075352aa1ccb8b1ababf8
marinagrebneva <sokovets@outlook.com>
2022-10-26 23:11:54 +0200

    added code with dynamic typing

---
+++ test1.js
@@ -0,0 +1,13 @@
+let ageString = "28";
+let age = 45;
+let stringAddition;
+let subtraction;
+
+console.log(typeof(ageString));
+
+stringAddition = ageString + age;
+subtraction = age - ageString;
+
+console.log(stringAddition);
+console.log(subtraction);
+console.log(typeof(subtraction));
