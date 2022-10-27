b9057b3e03a4c667d23850409769884a0977ac49
marinagrebneva <sokovets@outlook.com>
2022-10-27 17:01:41 +0200

    average score calculation

---
+++ task_score.js
@@ -0,0 +1,13 @@
+let pointsIntroduction;
+let pointsGit;
+let pointsJs;
+let averageScore;
+let fullStudentName;
+
+pointsIntroduction = 150;
+pointsGit = 425;
+pointsJs = 621;
+fullStudentName = "Petrov Ivan Olegovich";
+averageScore = Math.round((pointsIntroduction + pointsGit + pointsJs) / 3);
+console.log(`Student progress: ${fullStudentName} \n
+            Average score for all modules: ${averageScore}`);
