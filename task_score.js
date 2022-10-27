b69a6ed4be5818450ca1f98c592fa4ff84fbe205
marinagrebneva <sokovets@outlook.com>
2022-10-27 21:31:35 +0200

    added slice method

--- task_score.js
+++ task_score.js
@@ -9,5 +9,11 @@ pointsGit = 425;
 pointsJs = 621;
 fullStudentName = "Petrov Ivan Olegovich";
 averageScore = Math.round((pointsIntroduction + pointsGit + pointsJs) / 3);
-console.log(`Student progress: ${fullStudentName} \n
+console.log(`Student progress: ${fullStudentName}
+Average score for all modules: ${averageScore}`);
+
+
+//Вывод с сокращенным именем
+
+console.log(`Student progress: ${fullStudentName.slice(0, 1)} 
 Average score for all modules: ${averageScore}`);
